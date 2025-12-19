49b40b0f80ee228548be03177f62eff827ad577c
mrgrinsven <mrgrinsven@gmail.com>
2025-12-19 06:16:37 +0100

    first commit

---
+++ src/main/java/nl/novi/eindopdracht/backenderpsysteem/BackendErpSysteemApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.backenderpsysteem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendErpSysteemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendErpSysteemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/eindopdracht/backenderpsysteem/BackendErpSysteemApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.backenderpsysteem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendErpSysteemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
